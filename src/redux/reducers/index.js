becec332c2526090cddd5116ae266ffb517ac83e
somoye123 <somoye.ayotunde@gmail.com>
2020-12-16 20:55:04 +0100

    add sideBar reducer to Application combined reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,9 +3,12 @@ import Cart from "./cartReducer";
 import Filter from "./filterReducer";
 import Products from "./productsReducer";
 import User from "./userReducer";
+import SideBar from "./sideBarReducer";
+
 export default combineReducers({
   Cart,
   Filter,
   Products,
   User,
+  SideBar,
 });
