951874b548095caff18391507a6eae8b5b35c6b1
taiwocoker <taiwofcoker@gmail.com>
2020-12-18 16:51:15 +0100

    Build application contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,31 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-const Contact = () => <h4>contact section</h4>;
+const Contact = () => (
+  <Wrapper>
+    <div className='section-center'>
+      <h3>Join our newsletter and get 20% off</h3>
+      <div className='content'>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
+          alias voluptatibus, repudiandae nisi laboriosam sequi rerum officiis
+          earum voluptatem.
+        </p>
+        <form className='contact-form'>
+          <input
+            type='email'
+            placeholder='enter your email'
+            className='form-input'
+          />
+          <button type='submit' className='submit-btn'>
+            Subscribe
+          </button>
+        </form>
+      </div>
+    </div>
+  </Wrapper>
+)
+
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -65,6 +89,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`;
+`
 
-export default Contact;
+export default Contact
