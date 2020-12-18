c6f40ad4a880de51de29c148903d830022bec3d0
taiwocoker <taiwofcoker@gmail.com>
2020-12-18 10:30:19 +0100

    Update the component with props

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
-const PageHero = () => <h4>page hero</h4>;
+const PageHero = ({ title }) => (
+  <Wrapper>
+    <div className='section-center'>
+      <h3>
+        <Link to='/'>Home</Link>/ {title}
+      </h3>
+    </div>
+  </Wrapper>
+)
 
 const Wrapper = styled.section`
   background: var(--clr-primary-10);
@@ -20,6 +28,6 @@ const Wrapper = styled.section`
   a:hover {
     color: var(--clr-primary-1);
   }
-`;
+`
 
-export default PageHero;
+export default PageHero
