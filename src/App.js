96b5f216bd488235251d27d5e3cb22dfc689aae4
taiwocoker <taiwofcoker@gmail.com>
2020-12-19 00:25:41 +0100

    Load products on initial application load

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Sidebar, Footer } from './components';
+import {loadProducts} from './redux/actions/productsAction'
 import {Home, About, Error, Checkout, Products, SingleProduct, PrivateRoute, Cart} from './pages'
 
+
 function App() {
+  React.useEffect(() => {
+    loadProducts()
+  }, [])
   return (
     <Router>
       <Navbar />
