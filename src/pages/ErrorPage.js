ede3e3b93f2f000d11e24a5a395bdeafc484db11
taiwocoker <taiwofcoker@gmail.com>
2020-12-18 10:00:46 +0100

    Build application error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -2,7 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const ErrorPage = () => <h4>error page</h4>;
+const ErrorPage = () => {
+  return (
+    <Wrapper className="page-100">
+      <section>
+        <h1>404</h1>
+        <h3>Sorry, the page you tried cannot be found</h3>
+        <Link to="/" className="btn">
+          Back Home
+        </Link>
+      </section>
+    </Wrapper>
+  )
+}
 
 const Wrapper = styled.main`
   background: var(--clr-primary-10);
