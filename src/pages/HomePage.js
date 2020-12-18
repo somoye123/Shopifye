580aa50275114549113353558bf5781f0d59f94f
somoye123 <somoye.ayotunde@gmail.com>
2020-12-18 10:55:51 +0100

    build appliation homePage structure

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import {
-  FeaturedProducts, Hero, Services, Contact,
-} from '../components';
+import React from "react";
+import { FeaturedProducts, Hero, Services, Contact } from "../components";
 
-const HomePage = () => <h4>home page</h4>;
+const HomePage = () => (
+  <main>
+    <Hero />
+    <FeaturedProducts />
+    <Services />
+    <Contact />
+  </main>
+);
 
 export default HomePage;
