b9ae2c35d6968772f9b60d8cd23b4168e51ad101
taiwocoker <taiwofcoker@gmail.com>
2020-12-18 10:29:08 +0100

    Build About page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import styled from 'styled-components';
-import { PageHero } from '../components';
-import aboutImg from '../assets/hero-bcg.jpeg';
+import React from 'react'
+import styled from 'styled-components'
+import { PageHero } from '../components'
+import aboutImg from '../assets/hero-bcg.jpeg'
 
-const AboutPage = () => <h4>about page</h4>;
+const AboutPage = () => (
+  <main>
+    <PageHero title='about' />
+    <Wrapper className='page section section-center'>
+      <img src={aboutImg} alt='nice-desk' />
+      <article>
+        <div className='title'>
+          <h2>Our story</h2>
+          <div className='underline'></div>
+        </div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum neque
+          tempora placeat aspernatur impedit nesciunt nulla quidem voluptatibus
+          consectetur repellat.
+        </p>
+      </article>
+    </Wrapper>
+  </main>
+)
 
 const Wrapper = styled.section`
   display: grid;
@@ -31,5 +49,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`;
-export default AboutPage;
+`
+export default AboutPage
