dda46640e620487cbc65221ed71c9d1b707b9445
somoye123 <somoye.ayotunde@gmail.com>
2020-12-20 21:06:45 +0100

    update products reducer to accomodate more action types

--- src/redux/reducers/productsReducer.js
+++ src/redux/reducers/productsReducer.js
@@ -1,7 +1,23 @@
-import * as types from "../actions/actionTypes";
-import initialState from "./initialState";
+import * as types from '../actions/actionTypes';
+import initialState from './initialState';
 
 export default (state = initialState.products, action) => {
-  return state;
-  throw new Error(`No Matching "${action.type}" - action type`);
+  switch (action.type) {
+    case types.GET_PRODUCTS_BEGIN:
+      return { ...state, products_loading: true };
+    case types.GET_PRODUCTS_SUCCESS:
+      const featured_products = action.products.filter(
+        (product) => product.featured === true
+      );
+      return {
+        ...state,
+        products_loading: false,
+        products: action.products,
+        featured_products,
+      };
+    case types.GET_PRODUCTS_ERROR:
+      return { ...state, products_loading: false, products_error: true };
+    default:
+      return state;
+  }
 };
