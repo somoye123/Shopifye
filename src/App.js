9d87d0557eac1b50df586aa119be83650f7468c8
somoye123 <somoye.ayotunde@gmail.com>
2020-12-20 21:09:22 +0100

    connect app component to redux

--- src/App.js
+++ src/App.js
@@ -1,42 +1,53 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Sidebar, Footer } from './components';
-import {loadProducts} from './redux/actions/productsAction'
-import {Home, About, Error, Checkout, Products, SingleProduct, PrivateRoute, Cart} from './pages'
+import { fetchProducts } from './redux/actions/productsAction';
+import { connect } from 'react-redux';
+import {
+  Home,
+  About,
+  Error,
+  Checkout,
+  Products,
+  SingleProduct,
+  PrivateRoute,
+  Cart,
+} from './pages';
 
-
-function App() {
+function App({ fetchProducts }) {
   React.useEffect(() => {
-    loadProducts()
-  }, [])
+    fetchProducts();
+  }, []);
   return (
     <Router>
       <Navbar />
       <Sidebar />
       <Switch>
-        <Route exact path='/'>
+        <Route exact path="/">
           <Home />
         </Route>
-        <Route exact path='/about'>
+        <Route exact path="/about">
           <About />
         </Route>
-        <Route exact path='/cart'>
+        <Route exact path="/cart">
           <Cart />
         </Route>
-        <Route exact path='/checkout'>
+        <Route exact path="/checkout">
           <Checkout />
         </Route>
-        <Route exact path='/products'>
+        <Route exact path="/products">
           <Products />
         </Route>
-        <Route exact path='/products/:id' children={<SingleProduct />} />
-        <Route exact path='*'>
+        <Route exact path="/products/:id" children={<SingleProduct />} />
+        <Route exact path="*">
           <Error />
         </Route>
       </Switch>
       <Footer />
     </Router>
-  )
+  );
 }
 
-export default App;
+const mapDispatchToProps = { fetchProducts };
+
+export default connect(null, mapDispatchToProps)(App);
