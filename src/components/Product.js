20233efa1365305bc45f21f0bce8b493fd7b1fcc
taiwocoker <taiwofcoker@gmail.com>
2020-12-20 23:41:18 +0100

    Build application product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import styled from 'styled-components';
-import { FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import { formatPrice } from '../utils/helpers';
+import React from 'react'
+import styled from 'styled-components'
+import { FaSearch } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import { formatPrice } from '../utils/helpers'
 
-const Product = () => <h4>product</h4>;
+const Product = ({ product: { image, name, price, id } }) => {
+  return (
+    <Wrapper>
+      <div className='container'>
+        <img src={image} alt={name} />
+        <Link to={`/product/${id}`} className='link'>
+          <FaSearch />
+        </Link>
+      </div>
+      <footer>
+        <h5>{name}</h5>
+        <p>{price}</p>
+      </footer>
+    </Wrapper>
+  )
+}
 
 const Wrapper = styled.article`
   .container {
@@ -61,5 +76,5 @@ const Wrapper = styled.article`
     color: var(--clr-primary-5);
     letter-spacing: var(--spacing);
   }
-`;
-export default Product;
+`
+export default Product
