1bd7de22eed3884c15b2ad9af1e1cb39112406fe
taiwocoker <taiwofcoker@gmail.com>
2020-12-20 23:44:04 +0100

    Build application featured product component

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import { useProductsContext } from '../context/products_context';
-import Error from './Error';
-import Loading from './Loading';
-import Product from './Product';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import Error from './Error'
+import Loading from './Loading'
+import Product from './Product'
+import { connect } from 'react-redux'
 
-const FeaturedProducts = () => <h4>featured products</h4>;
+const FeaturedProducts = ({ Products }) => {
+  const {
+    products_loading: loading,
+    products_error: error,
+    featured_products: featured,
+  } = Products
+
+  if (loading) return <Loading />
+
+  if (error) return <Error />
+  return (
+    <Wrapper className='section'>
+      <div className='title'>
+        <h2>Featured Products</h2>
+        <div className='underline'></div>
+        <div className='section-center featured'>
+          {featured.slice(0, 3).map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+    </Wrapper>
+  )
+}
 
 const Wrapper = styled.section`
   background: var(--clr-grey-10);
@@ -29,6 +52,8 @@ const Wrapper = styled.section`
       grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
     }
   }
-`;
+`
 
-export default FeaturedProducts;
+const mapStateToProps = ({ Products }) => ({ Products })
+
+export default connect(mapStateToProps)(FeaturedProducts)
