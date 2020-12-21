c728cfa7698ea6c037bd8bebf420a81bac00874d
somoye123 <somoye.ayotunde@gmail.com>
2020-12-21 11:03:26 +0100

    add fetchSingleProduct action creator

--- src/redux/actions/productsAction.js
+++ src/redux/actions/productsAction.js
@@ -13,3 +13,16 @@ export function fetchProducts() {
     }
   };
 }
+
+export function fetchSingleProduct(url) {
+  return async (dispatch) => {
+    dispatch({ type: types.GET_SINGLE_PRODUCT_BEGIN });
+    try {
+      const response = await productApi.fetchSingleProduct(url);
+      const singleProduct = response;
+      dispatch({ type: types.GET_SINGLE_PRODUCT_SUCCESS, singleProduct });
+    } catch (error) {
+      dispatch({ type: types.GET_SINGLE_PRODUCT_ERROR });
+    }
+  };
+}
