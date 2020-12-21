6544fc1ab71537ec6d392658e84428334ecb9388
somoye123 <somoye.ayotunde@gmail.com>
2020-12-21 16:00:39 +0100

    build color section of add to cart component

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -5,7 +5,32 @@ import { FaCheck } from 'react-icons/fa';
 import { useCartContext } from '../context/cart_context';
 import AmountButtons from './AmountButtons';
 
-const AddToCart = () => <h4>addToCart </h4>;
+const AddToCart = ({ product: { id, stock, colors } }) => {
+  const [mainColor, setMainColor] = useState(colors[0]);
+  return (
+    <Wrapper>
+      <div className="colors">
+        <span> colors : </span>
+        <div>
+          {colors.map((color, index) => {
+            return (
+              <button
+                key={index}
+                style={{ background: color }}
+                className={`${
+                  mainColor === color ? 'color-btn active' : 'color-btn'
+                }`}
+                onClick={() => setMainColor(color)}
+              >
+                {mainColor === color ? <FaCheck /> : null}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   margin-top: 2rem;
